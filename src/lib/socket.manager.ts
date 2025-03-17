775fa7acd109c198bc10734ca1af60b67c4a1bcf
ananddub <duanand6@gmail.com>
2025-03-17 18:49:34 +0530

    changed controllers logic

--- src/lib/socket.manager.ts
+++ src/lib/socket.manager.ts
@@ -11,8 +11,22 @@ export const initSocket = async (server: any = null): Promise<Server> => {
         console.log('Server value is needed ', server);
         throw 'Server value is needed';
     }
+    const pubClient = new Redis(Config.REDIS as any, Config.REDIS_PORT as string);
+    const subClient = pubClient.duplicate();
+    pubClient.on('connect', () => {
+        console.log('Redis Pub Client Connected');
+    });
+
+    pubClient.on('error', (err) => {
+        console.log('Redis Pub Client Error:', err);
+    });
+
+    subClient.on('error', (err) => {
+        console.log('Redis Sub Client Error:', err);
+    });
 
     io = new Server(server, {
+        adapter: createAdapter(pubClient, subClient),
         cors: {
             origin: '*',
             methods: ['*'],
